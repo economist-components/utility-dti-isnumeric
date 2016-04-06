c08df6d3efd07a650bbcf45329bf88626f3b3504
Evan Hensleigh <futuraprime@gmail.com>
2016-04-06 18:52:15 +0100

    fix: package.json error

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@
  * This function is taken directly from jQuery:
  * https://github.com/jquery/jquery/blob/c869a1ef8a031342e817a2c063179a787ff57239/src/core.js#L214
  *
- * @param  {*}  n - a value to be tested
+ * @param  {*}  testVal - a value to be tested
  *
  * @return {Boolean} whether it is numeric
  */
-export default function isNumeric(n) {
+export default function isNumeric(testVal) {
   // parseFloat NaNs numeric-cast false positives (null|true|false|"")
   // ...but misinterprets leading-number strings, particularly hex literals ("0x...")
   // subtraction forces infinities to NaN
   // adding 1 corrects loss of precision from parseFloat (#15100)
-  return !Array.isArray( n ) && (n - parseFloat( n ) + 1) >= 0;
+  return !Array.isArray(testVal) && (testVal - parseFloat(testVal) + 1) >= 0;
 }
--- test/index.js
+++ test/index.js
@@ -1,7 +1,12 @@
 import 'babel-polyfill';
-import isNumeric from '../src';
 import chai from 'chai';
+import isNumeric from '../src';
 chai.should();
+
+function dummyFunction() {
+  'I do nothing';
+}
+
 describe('isNumeric', () => {
   it('should detect numbers', () => {
     // numbers
@@ -15,9 +20,9 @@ describe('isNumeric', () => {
     // not numbers
     isNumeric('cartoon').should.be(false);
     isNumeric('').should.be(false);
-    isNumeric([2,4]).should.be(false);
-    isNumeric({ foo : 'bar' }).should.be(false);
-    isNumeric(function() {}).should.be(false);
+    isNumeric([ 2, 4 ]).should.be(false);
+    isNumeric({ foo: 'bar' }).should.be(false);
+    isNumeric(dummyFunction).should.be(false);
     isNumeric(NaN).should.be(false);
   });
 });
