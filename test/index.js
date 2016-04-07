d6ca02776f9460ba6141cfffb9eadef04a6856ce
Evan Hensleigh <futuraprime@gmail.com>
2016-04-07 17:30:51 +0100

    fix: And now they pass.

--- test/index.js
+++ test/index.js
@@ -1,30 +1,28 @@
 import 'babel-polyfill';
 import chai from 'chai';
-// import isNumeric from '../src';
+import isNumeric from '../src';
 chai.should();
 
-const expect = chai.expect;
-// function dummyFunction() {
-//   'I do nothing';
-// }
+function dummyFunction() {
+  'I do nothing';
+}
 
 describe('isNumeric', () => {
   it('should detect numbers', () => {
     // numbers
-    expect(true).to.be(true);
-    // expect(isNumeric(5)).to.be(true);
-    // isNumeric(-20.2).should.be(true);
-    // isNumeric(0).should.be(true);
-    // isNumeric(Math.PI).should.be(true);
-    // isNumeric('5').should.be(true);
-    // isNumeric('-20.2').should.be(true);
+    isNumeric(5).should.equal(true);
+    isNumeric(-20.2).should.equal(true);
+    isNumeric(0).should.equal(true);
+    isNumeric(Math.PI).should.equal(true);
+    isNumeric('5').should.equal(true);
+    isNumeric('-20.2').should.equal(true);
 
     // not numbers
-    // isNumeric('cartoon').should.be(false);
-    // isNumeric('').should.be(false);
-    // isNumeric([ 2, 4 ]).should.be(false);
-    // isNumeric({ foo: 'bar' }).should.be(false);
-    // isNumeric(dummyFunction).should.be(false);
-    // isNumeric(NaN).should.be(false);
+    isNumeric('cartoon').should.equal(false);
+    isNumeric('').should.equal(false);
+    isNumeric([ 2, 4 ]).should.equal(false);
+    isNumeric({ foo: 'bar' }).should.equal(false);
+    isNumeric(dummyFunction).should.equal(false);
+    isNumeric(NaN).should.equal(false);
   });
 });
